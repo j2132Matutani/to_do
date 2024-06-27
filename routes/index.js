7c0278ad4d57b73b96342eeac16da1749754225b
64you <142720062+64you@users.noreply.github.com>
2024-06-27 07:00:48 +0000

    const knexを追加

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pg = require('pg');
+const knex = require('../db/knex');
 
 const connection = new pg.Pool({
   host: 'localhost',
