68e6d556b25944d834d82d1e076771d1457cfee7
0uta <166687901+0uta@users.noreply.github.com>
2024-06-27 07:17:44 +0000

    第15章の途中

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
-const pg = require('pg');
+//const pg = require('pg');
 
-const connection = new pg.Pool({
-  host: 'localhost',
-  user: 'root',
-  password: 'postgres',
-  database: 'todo_app',
-  port: 5432,
-  });
+//const connection = new pg.Pool({
+  //host: 'localhost',
+  //user: 'root',
+  //password: 'postgres',
+  //database: 'todo_app',
+  //port: 5432,
+  //});
 
 let todos = [];
 
@@ -32,18 +32,48 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  const todo = req.body.add;
-  knex("tasks")
-    .insert({user_id: 1, content: todo})
-    .then(function () {
-      res.redirect('/')
+  const username = req.body.username;
+  const password = req.body.password;
+  const repassword = req.body.repassword;
+
+  knex("users")
+    .where({name: username})
+    .select("*")
+    .then(function (result) {
+      if (result.length !== 0) {
+        res.render("signup", {
+          title: "Sign up",
+          errorMessage: ["このユーザ名は既に使われています"],
+        }) 
+      } else if (password === repassword) {
+        knex("users")
+          .insert({name: username, password: password})
+          .then(function () {
+            res.redirect("/");
+          })
+          .catch(function (err) {
+            console.error(err);
+            res.render("signup", {
+              title: "Sign up",
+              errorMessage: [err.sqlMessage],
+            });
+          });
+      } else {
+        res.render("signup", {
+          title: "Sign up",
+          errorMessage: ["パスワードが一致しません"],
+        });
+      }
     })
     .catch(function (err) {
       console.error(err);
-      res.render('index', {
-        title: 'ToDo App',
+      res.render("signup", {
+        title: "Sign up",
+        errorMessage: [err.sqlMessage],
       });
     });
 });
+
+router.use('/signup', require('./signup'));
 
 module.exports = router;
