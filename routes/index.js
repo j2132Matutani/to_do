e079395e25ed4f2437f1120d9572226fd6bcaf4f
0uta <166687901+0uta@users.noreply.github.com>
2024-07-04 07:12:49 +0000

    15章の終わり際

--- routes/index.js
+++ routes/index.js
@@ -1,79 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
-//const pg = require('pg');
-
-//const connection = new pg.Pool({
-  //host: 'localhost',
-  //user: 'root',
-  //password: 'postgres',
-  //database: 'todo_app',
-  //port: 5432,
-  //});
-
-let todos = [];
 
 router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   knex("tasks")
     .select("*")
     .then(function (results) {
-      console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
+        isAuth: isAuth,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
 
 router.post('/', function (req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-  const repassword = req.body.repassword;
-
-  knex("users")
-    .where({name: username})
-    .select("*")
-    .then(function (result) {
-      if (result.length !== 0) {
-        res.render("signup", {
-          title: "Sign up",
-          errorMessage: ["このユーザ名は既に使われています"],
-        }) 
-      } else if (password === repassword) {
-        knex("users")
-          .insert({name: username, password: password})
-          .then(function () {
-            res.redirect("/");
-          })
-          .catch(function (err) {
-            console.error(err);
-            res.render("signup", {
-              title: "Sign up",
-              errorMessage: [err.sqlMessage],
-            });
-          });
-      } else {
-        res.render("signup", {
-          title: "Sign up",
-          errorMessage: ["パスワードが一致しません"],
-        });
-      }
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  const todo = req.body.add;
+  knex("tasks")
+    .insert({user_id: 1, content: todo})
+    .then(function () {
+      res.redirect('/')
     })
     .catch(function (err) {
       console.error(err);
-      res.render("signup", {
-        title: "Sign up",
-        errorMessage: [err.sqlMessage],
+      res.render('index', {
+        title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;
