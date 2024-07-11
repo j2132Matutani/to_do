956b603233f8048e17260842d83d743e4412279d
64you <142720062+64you@users.noreply.github.com>
2024-07-11 06:47:05 +0000

    15章の内容をすべて実装

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const knex = require('../db/knex');
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  console.log(`isAuth: ${isAuth}`);
+  //console.log(`isAuth: ${isAuth}`);
   knex("tasks")
     .select("*")
     .then(function (results) {
@@ -13,20 +13,24 @@ router.get('/', function (req, res, next) {
       res.render('index', {
         title: 'ToDo App',
         todos: results,
+        isAuth: isAuth,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
 
 router.post('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   const todo = req.body.add;
   knex("tasks")
-    .insert({user_id: 1, content: todo})
+    .insert({user_id: userId, content: todo})
     .then(function () {
       res.redirect('/')
     })
@@ -34,11 +38,13 @@ router.post('/', function (req, res, next) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        isAuth: isAuth,
       });
     });
 });
 
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 
 module.exports = router;
