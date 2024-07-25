a6a8c0b689e46e225710720e9453a207d3aaf203
Toru Kurahashi <tkurahashi@matsue-ct.ac.jp>
2024-07-25 18:18:46 +0900

    Update signin.js for req.flash error

--- routes/signin.js
+++ routes/signin.js
@@ -14,7 +14,7 @@ router.get('/', function (req, res, next) {
 router.post('/', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/signin',
-    failureFlash: true,
+    failureFlash: false,
   }
 ));
 
